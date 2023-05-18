59779d9a5f96b6433f0f1820f7c0bb0708e53314
Jay Dave <davejay3002@gmail.com>
2023-05-18 16:57:36 +0530

    Solved the MongoDB error

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,32 +10,34 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-    session.user.id = sessionUser._id.toString();
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      //serverless  -> Lambda -> dynamodb
-      await connectToDB();
-      // check if a user already exists
-      const UserExists = await User.findOne({ email: profile.email });
-      // if not, create a new user
-      if (!UserExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        //serverless  -> Lambda -> dynamodb
+        await connectToDB();
+        // check if a user already exists
+        const UserExists = await User.findOne({ email: profile.email });
+        // if not, create a new user
+        if (!UserExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
       }
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
